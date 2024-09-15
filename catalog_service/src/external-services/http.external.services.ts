980e98fe54fa76143b65ae685824591bd4c59dcf
sing3demons <sing3demons@live.com>
2024-09-15 17:31:46 +0700

    Refactor HttpService to use logger from logger module and add transactionId support

--- catalog_service/src/external-services/http.external.services.ts
+++ catalog_service/src/external-services/http.external.services.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosBasicCredentials, AxiosError } from 'axios'
 import axiosRetry from 'axios-retry'
 import https from 'https'
-import { HttpLogger } from '../logger'
+import { logger as Logger } from '../logger'
+import { v7 as uuid } from 'uuid'
+import { Request } from 'express'
 
 type ConfigService = {
     endpoint: string
@@ -19,40 +21,47 @@ type HttpResponse<T> = {
     message: string
 }
 
-export class HttpServiceServer {
+type HttpServiceOptions = {
+    headers?: Record<string, string>
+    req?: Request
+    auth?: AxiosBasicCredentials
+}
+
+export class HttpService {
     constructor(
         private readonly configService: ConfigService,
-        private readonly logger: HttpLogger = new HttpLogger({ headers: {} } as any)
+        private readonly _logger = Logger
     ) { }
-    async get<TParams, TResponse>(data: TParams, headers: Record<string, string> = {}): Promise<HttpResponse<TResponse>> {
+    async get<TParams, TResponse>(data: TParams, option: HttpServiceOptions, transactionId: string = `${uuid()}`): Promise<HttpResponse<TResponse>> {
         const result: HttpResponse<TResponse> = {
             err: true,
             data: {} as TResponse,
             status: 500,
             message: ''
         }
+        const logger = this._logger.child({ transactionId: option.req?.headers['x-transaction-id'] || transactionId })
         if (this.configService.retries) {
             axiosRetry(axios, {
                 retries: this.configService.retries,
                 retryDelay: (retryCount) => {
-                    this.logger?.error(`Retry attempt: ${retryCount}`)
+                    logger.error(`Retry attempt: ${retryCount}`)
                     return retryCount * 1000; // Time in ms, e.g., 1000ms = 1s
                 },
                 retryCondition: (error) => {
-                    this.logger?.error(`Retry condition: ${error.code} ${error.response?.status} ${error.response?.data}`)
+                    logger.error(`Retry condition: ${error.code} ${error.response?.status} ${error.response?.data}`)
                     return error.code === 'ECONNABORTED' || error.code === 'ECONNREFUSED' || error.code === 'ECONNRESET' || error.response?.status === 429 || error.response?.status === 503
                 },
             })
         }
         const config: AxiosRequestConfig<TParams> = {
             method: 'get',
-            headers: this.configService.headers ? this.configService.headers : headers,
-            auth: this.configService.auth,
+            headers: this.configService.headers ? this.configService.headers : option.headers || {},
+            auth: this.configService.auth || option.auth,
             timeout: this.configService.timeout,
             params: data,
             httpsAgent: this.configService.httpsAgent ? new https.Agent(this.configService.httpsAgent) : {},
         }
-        if (!this.configService.auth) {
+        if (config.auth && Object.keys(config.auth).length === 0) {
             delete config.auth
         }
         if (!this.configService.timeout) {
@@ -67,29 +76,29 @@ export class HttpServiceServer {
             delete config.httpsAgent
         }
 
-        this.logger?.info(config, `config axios`)
+        logger.info(config, `config axios`)
         try {
             const response = await axios.request<TParams, AxiosResponse<TResponse, TResponse>, TParams>(config)
-            this.logger?.info(response, `response axios`)
+            logger.info(response, `response axios`)
             result.err = false
             result.data = response.data
             result.status = response.status
             return result
         } catch (error) {
             if (error instanceof AxiosError) {
-                this.logger?.error(error, `AxiosError`)
+                logger.error(error, `AxiosError`)
                 result.err = true
                 result.message = error.message
                 result.status = error.response?.status || 500
                 result.data = error.response?.data || {} as TResponse
 
             } else if (error instanceof Error) {
-                this.logger?.error(error, `Error`)
+                logger.error(error, `Error`)
                 result.err = true
                 result.message = error.message
 
             } else {
-                this.logger?.error(error, `error`)
+                logger.error(error, `error`)
                 result.err = true
                 result.message = 'unknown error'
             }
@@ -97,29 +106,33 @@ export class HttpServiceServer {
         }
     }
 
-    async post<TBody, TResponse>(data: TBody, headers: Record<string, string> = {}): Promise<HttpResponse<TResponse>> {
+    async post<TBody, TResponse>(data: TBody, option: HttpServiceOptions, transactionId: string = `${uuid()}`): Promise<HttpResponse<TResponse>> {
         const result: HttpResponse<TResponse> = {
             err: true,
             data: {} as TResponse,
             status: 500,
             message: ''
         }
+
+        const logger = this._logger.child({ transactionId: option.req?.headers['x-transaction-id'] || transactionId })
+
+
         if (this.configService.retries) {
             axiosRetry(axios, {
                 retries: this.configService.retries,
                 retryDelay: (retryCount) => {
-                    this.logger?.error(`Retry attempt: ${retryCount}`)
+                    logger.error(`Retry attempt: ${retryCount}`)
                     return retryCount * 1000;
                 },
                 retryCondition: (error) => {
-                    this.logger?.error(error, `Retry condition`)
+                    logger.error(error, `Retry condition`)
                     return error.code === 'ECONNABORTED' || error.code === 'ECONNREFUSED' || error.code === 'ECONNRESET' || error.response?.status === 429 || error.response?.status === 503
                 },
             })
         }
         const config: AxiosRequestConfig<TBody> = {
             method: 'post',
-            headers: this.configService.headers ? this.configService.headers : headers,
+            headers: this.configService.headers ? this.configService.headers : option.headers || {},
             auth: this.configService.auth,
             timeout: this.configService.timeout,
             data,
@@ -140,29 +153,29 @@ export class HttpServiceServer {
             delete config.httpsAgent
         }
 
-        this.logger?.info(config, `config axios`)
+        logger.info(config, `config axios`)
         try {
             const response = await axios.request<TBody, AxiosResponse<TResponse, TResponse>, TBody>(config)
-            this.logger?.info(response, `response axios`)
+            logger.info(response, `response axios`)
             result.err = false
             result.data = response.data
             result.status = response.status
             return result
         } catch (error) {
             if (error instanceof AxiosError) {
-                this.logger?.error(error, `AxiosError`)
+                logger.error(error, `AxiosError`)
                 result.err = true
                 result.message = error.message
                 result.status = error.response?.status || 500
                 result.data = error.response?.data || {} as TResponse
 
             } else if (error instanceof Error) {
-                this.logger?.error(error, `Error`)
+                logger.error(error, `Error`)
                 result.err = true
                 result.message = error.message
 
             } else {
-                this.logger?.error(error, `error`)
+                logger.error(error, `error`)
                 result.err = true
                 result.message = 'unknown error'
             }
